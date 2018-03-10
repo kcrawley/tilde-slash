8f46474fe53d004dead20c09dff661e51ff69e4a
kevin crawley <kevin@singlemusic.co>
2018-03-10 15:57:38 -0600

    change linux script

--- .setup-linux.sh
+++ .setup-linux.sh
@@ -2,9 +2,11 @@
 
 curl -L git.io/antigen > antigen.zsh
 
+cd $HOME && rm -rf .git
+
 git init
-git remote add origin git@github.com:kcrawley/tilde-slash.git
+git remote add origin https://github.com/kcrawley/tilde-slash.git
 git fetch
 git reset --hard origin/master
 
-
+git remote set-url origin git@github.com:kcrawley/tilde-slash.git
